79110b0be5aa8723305e6c1f0e35188cca32cf34
Jean <jeansfloresof@gmail.com>
2019-08-17 21:52:06 -0300

    Adiciona ferramentas de padronização de código

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello'})
-})
+routes.get('/', (req, res) => res.json({ message: 'Hello' }));
 
-module.exports = routes
+module.exports = routes;
