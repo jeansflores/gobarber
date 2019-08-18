7609a99da06ae92169183434d7ddee1cc66b20d0
Jean <jeansfloresof@gmail.com>
2019-08-18 05:18:49 -0300

    Adiciona rota de criação de usuário

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'teste',
-    email: 'asdf',
-    password_hash: 'hkgsdf',
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
-
-module.exports = routes;
+export default routes;
