36e395532e2218293ec4c642750745dba1021cd0
Jean <jeansfloresof@gmail.com>
2019-08-19 22:11:50 -0300

    Adiciona upload de imagem

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
-
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 routes.put('/users', UserController.update);
 
