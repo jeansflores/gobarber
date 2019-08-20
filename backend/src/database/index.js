fe30e93f1047ad08842cce90aca662940e805f30
Jean <jeansfloresof@gmail.com>
2019-08-19 23:43:03 -0300

    Configura o mongodb

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 
+import mongoose from 'mongoose';
 import File from '../app/models/File';
 import User from '../app/models/User';
 import Appointment from '../app/models/Appointment';
@@ -10,6 +11,7 @@ const models = [File, User, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -18,6 +20,16 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
+    );
   }
 }
 
