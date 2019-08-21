0d27dd75a107871b86b83671a94ed13445095454
Jean <jeansfloresof@gmail.com>
2019-08-21 00:30:13 -0300

    Configura variáveis de ambientes

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
-  secure: false,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: process.env.MAIL_SECURE,
   auth: {
-    user: '7486a01e8545e0',
-    pass: '47c9eae41f1d15',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Team GoBarber <noreply@gobarber.com>',
